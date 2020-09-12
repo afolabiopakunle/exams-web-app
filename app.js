fedebc693a9eca1d3ad35b09f18db4a0e367d843
afolabiopakunle <opakunleafolabi@gmail.com>
2020-09-12 21:44:01 +0100

    app working perfectly

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ let option4 = document.getElementById("opt4")
 let questionBox = document.getElementById("question")
 let nextButton = document.getElementById("nextBtn");
 let result = document.getElementById("result")
-
+let totalQuestions = questions.length;
 let currentQuestion = 0;
 let score = 0;
+let examContainer = document.querySelector("#examContainer")
 
 function loadQuestion(questionNumber) {
     let q = questions[questionNumber]
@@ -24,9 +25,28 @@ function loadNextQuestion() {
     let selectedOption = document.querySelector("input[type=radio]:checked");
     if (!selectedOption) {
         alert("You need to pick a question")
+        return
     }
+    let answer = selectedOption.value;
+    console.log("Doc answer: ", answer)
+    console.log("JS: ", questions[currentQuestion].answer)
+    if (questions[currentQuestion].answer == answer) {
+        score += 10;
+    }
+    selectedOption.checked = false;
+    currentQuestion++;
+    if (currentQuestion === totalQuestions - 1) {
+        nextButton.textContent = "Finish"
+    }
+    if (currentQuestion === totalQuestions) {
+        examContainer.style.display = "none";
+        result.style.display = ""
+        result.textContent = " Your Score is: " + score + " over " + totalQuestions * 10;
+        return
+    }
+    loadQuestion(currentQuestion)
 
 }
 
 
-loadQuestion(0)
+loadQuestion(currentQuestion)
