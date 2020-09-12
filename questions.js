daea2b7efdf59620360333d191c83006e4994773
afolabiopakunle <opakunleafolabi@gmail.com>
2020-09-12 10:38:52 +0100

    questions done

---
+++ questions.js
@@ -0,0 +1,91 @@
+let questions =
+    [{
+        question: "What is a CMS in web design?",
+        option1: "Content Management System",
+        option2: "Creative Management System",
+        option3: "Content Mixing System",
+        option4: "Creatives Managerial System",
+        answer: option1,
+    },
+    {
+        question: "To make your website mobile friendly, you can make your website?",
+        option1: "Reactive",
+        option2: "Responsive",
+        option3: "Fast Loading",
+        option4: "Light",
+        answer: option2
+    }]
+//     [
+//       3,
+//       "What does CSS stand for?",
+//       "Current Style Sheets",
+//       "Current Sheets Style",
+//       "Cascading Style Sheets",
+//       "Cascading Sheets Style",
+//       "Cascading Style Sheets"
+//     ],
+//     [
+//       4,
+//       "Which of the following statements is false?",
+//       "You can make a website without using HTML",
+//       "You can make a website without using PHP",
+//       " You can make a website without using Javascript",
+//       "You can make a website without using CSS",
+//       "You can make a website without using HTML",
+//     ],
+//     [
+//       5,
+//       "What is WordPress?",
+//       "It is a software used to press text",
+//       "It is a text formatting software",
+//       "It is a CMS (Content Management System)",
+//       "It is mail service",
+//       "It is a CMS (Content Management System)",
+//     ],
+//     [
+//       6,
+//       " Which of the following is true about Javascript?",
+//       "It is a server side scripting language",
+//       "It is client side scripting language",
+//       "It is a database",
+//       "It is a Software",
+//       "It is client side scripting language",
+//     ],
+//     [
+//       7,
+//       "SQL stands for?",
+//       "Structured Query Language",
+//       "Statistical Query Language",
+//       "Superior Questions Lot",
+//       "Standard Query Lot",
+//       "Structured Query Language"
+//     ],
+//     [
+//       8,
+//       "Which of the following is true?",
+//       "You need a server to host your website files",
+//       "You don’t need a server to host your website files",
+//       "You can create a website without using HTML",
+//       "You can’t create a website without a CMS",
+//       "You need a server to host your website files",
+//     ],
+//     [
+//       9,
+//       "Which of the following softwares could be used to build a website?",
+//       "Power Point",
+//       "Excel",
+//       "Dream Weaver",
+//       "ERP",
+//       "Dream Weaver",
+//     ],
+//     [
+//       10,
+//       "Which of the following is not a CMS?",
+//       "WordPress",
+//       "Drupal",
+//       "Magneto",
+//       "SAP",
+//       "SAP",
+//     ],
+//   ];
+// }
